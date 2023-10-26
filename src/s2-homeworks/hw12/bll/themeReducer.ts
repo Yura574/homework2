1d8b7008b2e14505287d26c4ca86621e83880056
Yura <yura5742248@gmail.com>
2023-10-26 14:17:15 +0300

    homework 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
-const initState = {
+const initState: InitialStateType = {
     themeId: 1,
 }
+type InitialStateType = {
+    themeId: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChaneThemeIdType): InitialStateType => { // fix any
+    console.log(action)
     switch (action.type) {
         // дописать
+
+        case 'SET_THEME_ID':
+            console.log('sdssd')
+            return {...state, themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChaneThemeIdType => ({type: 'SET_THEME_ID', id}) // fix any
+
+type ChaneThemeIdType = { type: string, id: number }
